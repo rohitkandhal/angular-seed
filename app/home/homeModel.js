75b3b9fff1d5b4c53bfafced2efc358b46c6f87c
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-06-28 13:11:54 -0400

    Added bootstrap css

--- app/home/homeModel.js
+++ app/home/homeModel.js
@@ -5,14 +5,57 @@ window.vjs = window.vjs || {};
     ns.HomeModel = HomeModel;
 
     var TYPES_PRIMITIVE = Object.freeze(
-        ['number', 'string', 'boolean', 'null', 'undefined']
-        );
+        [{
+            name: 'number'
+        }, {
+            name: 'string'
+        }, {
+            name: 'boolean'
+        }, {
+            name: 'null'
+        }, {
+            name: 'undefined'
+        }]);
 
     var TYPES_REFERENCE = Object.freeze(
-        ['Object', 'Function', 'Array', 'Number', 'String', 'Boolean', 'RegExp']
-        );
+        [ {
+            name: 'Object',
+            isInstanceOf: function(value) {
+                return value instanceof Object;
+            }
+        }, {
+            name: 'Function',
+            isInstanceOf: function(value) {
+                return value instanceof Function;
+            }
+        }, {
+            name: 'Array',
+            isInstanceOf: function(value) {
+                return value instanceof Array;
+            }
+        },  {
+            name: 'Number',
+            isInstanceOf: function(value) {
+                return value instanceof Number;
+            }
+        },  {
+            name: 'String',
+            isInstanceOf: function(value) {
+                return value instanceof String;
+            }
+        }, {
+            name: 'Boolean',
+            isInstanceOf: function(value) {
+                return value instanceof Boolean;
+            }
+        }, {
+            name: 'RegExp',
+            isInstanceOf: function(value) {
+                return value instanceof RegExp;
+            }
+        }]);
 
-    var COLORS = Object.freeze({
+    var RESULT_ENUM = Object.freeze({
             true: 'green',
             false: 'red',
             error: 'yellow'
@@ -29,7 +72,7 @@ window.vjs = window.vjs || {};
        var isSupported = false, i = 0;
 
        while(i < TYPES_PRIMITIVE.length && !isSupported) {
-        if(type === TYPES_PRIMITIVE[i]) {
+        if(type === TYPES_PRIMITIVE[i].name) {
             isSupported = true;
         }
         i += 1;
@@ -37,7 +80,7 @@ window.vjs = window.vjs || {};
 
         i = 0;
        while(i < TYPES_REFERENCE.length && !isSupported) {
-        if(type === TYPES_REFERENCE[i]) {
+        if(type === TYPES_REFERENCE[i].name) {
             isSupported = true;
         }
         i += 1;
@@ -47,15 +90,24 @@ window.vjs = window.vjs || {};
 
     var checkInstanceOf = function(inp, type){
        // returns if 'inp' is instance of 'type' 
-
-       // check if type is supported by app
-       var i = 0, isInstance = false;
-
+       var i = 0, result = false;
        if(isTypeSupported(type)) {
-            isInstance = inp instanceof type;
+            // For primitive types instanceof will always be false 
+            if(isObject(inp)) {
+                result = RESULT_ENUM[inp instanceof type];    
+            } else {
+                result = RESULT_ENUM[false];
+            }
+            
+       } else {
+            result = RESULT_ENUM.error;
        }
-       return isInstance;
+       return result;
     }    
+
+    function isObject(value) {
+        return value !== null && typeof value === 'object';
+    }
 
     HomeModel.prototype.checkInstanceOf = checkInstanceOf;
     
